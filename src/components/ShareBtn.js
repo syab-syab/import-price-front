629e0ff3c2749259fffd88b77b1d8590004dd454
syab-syab <syabsyabsyabsyab@gmail.com>
2023-08-30 22:10:53 +0900

    twitterカード追加

---
+++ src/components/ShareBtn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShareBtn = () => {
+  return (
+    <div>
+      <a href="https://unique-sprinkles-003d9b.netlify.app/&text=YEN$CONVERSION" target="blank"><span>Share</span></a>
+    </div>
+  )
+}
+
+export default ShareBtn
